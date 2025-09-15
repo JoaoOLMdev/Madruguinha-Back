b3740ef47b9fd50a3af2e940f5c97ceb8bc3d276
unknown <joaoolmdev@gmail.com>
2025-09-14 21:51:30 -0300

    edit: rename root directory

--- app/settings.py
+++ app/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'MadruguinhaBack.urls'
+ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MadruguinhaBack.wsgi.application'
+WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # Database
