62b6c23c19cfde6e25e4ae4dce6b8bb8bc4726b7
unknown <joaoolmdev@gmail.com>
2025-09-19 14:21:57 -0300

    feat: user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'phone_number', 'is_provider', 'is_staff', 'is_active']
+    list_filter = ['is_provider', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', 'adress', 'birth_date', 'is_provider')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone_number', 'adress', 'birth_date', 'is_provider')}),
+    )
+    search_fields = ['email', 'username']
+    ordering = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    adress = models.CharField(max_length=255, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    is_provider = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
